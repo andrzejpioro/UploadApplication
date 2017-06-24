753022e0a38e069f54db2a6e9a87e7592a665aeb
a.pioro <andrzej.pioro@gmail.com>
2017-06-24 23:47:30 +0200

    Poprawki

--- src/main/java/pl/pioro/dao/FileDao.java
+++ src/main/java/pl/pioro/dao/FileDao.java
@@ -1,15 +1,29 @@
 package pl.pioro.dao;
 
+import java.util.Date;
 import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import pl.pioro.model.UploadedFileDTO;
 
-public interface FileDao {
+@Transactional
+public interface FileNewDao extends CrudRepository<UploadedFileDTO, Long>{
 
-	public UploadedFileDTO insert(UploadedFileDTO file); 
-	public UploadedFileDTO delete(UploadedFileDTO file); 
-	public UploadedFileDTO getByUUID(String uuid);
 	
-	public List<UploadedFileDTO> getExiredFiles();
-	public List<UploadedFileDTO> getActiveFiles();
+	public UploadedFileDTO getByUuid(String uuid);
+	
+	@Query("select f from UploadedFileDTO f where f.fileExpr < ?1  and f.deleted = 0")
+	public List<UploadedFileDTO> getExiredFiles(Date data);
+	
+	@Query("select f from UploadedFileDTO f where (f.fileExpr > ?1 or f.fileExpr is null) and f.deleted = 0 ")
+	public List<UploadedFileDTO> getActiveFiles(Date data);
+	
+	@Modifying
+	@Query("UPDATE UploadedFileDTO set deleted = 1 where uuid= ?1 ")
+	void delete(String uuid);
 }
